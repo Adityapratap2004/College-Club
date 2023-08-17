a44cf7e40db0dcfe951af2f5ddd24f2af453025b
Aditya Pratap Singh <2004adityaprataps@gmail.com>
2023-08-17 16:03:16 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavBar from "./Components/NavBar";
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
+
+import Cards3 from "./Components/Cards3";
+import Quote from "./Components/Quote";
+import Footer from "./Components/Footer";
+import Clubs from './Components/Clubs';
+import Events from "./Components/Events";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css"
+
+
+import HeroSection from "./Components/HeroSection";
 
 function App() {
+
+  // const clubs = useRef(null);
+  // const footer = useRef(null);
+  // const scrollToSection = (element)=>{
+  //   window.scrollTo({
+  //     top: element.current.offsetTop,
+  //     behavior:'smooth',
+  //   })
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen max-w-full" id="popup-root" >
+      <BrowserRouter>
+        <Routes>
+
+
+        </Routes>
+
+        <NavBar ></NavBar>
+        <div className="top-section">
+        <HeroSection></HeroSection>
+        <Quote/>
+        </div>
+        <Cards3></Cards3>
+        <Events/>
+        
+        {/* <Carousel></Carousel> */}
+        <Clubs ></Clubs>
+        <Footer></Footer>
+        
+
+        
+
+        
+
+
+
+
+
+
+
+      </BrowserRouter>
+
     </div>
   );
 }
